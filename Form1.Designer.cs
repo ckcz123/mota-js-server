1abc4ee28176dba30067f1c2ba2a513390bfaaec
cafel176 <48304406+cafel176@users.noreply.github.com>
2019-11-29 22:42:24 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace mota_js_server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "正在启动服务...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 95);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "启动游戏";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(209, 95);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "地图编辑器";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button11);
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Location = new System.Drawing.Point(43, 190);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(277, 262);
+            this.panel1.TabIndex = 3;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(125, 122);
+            this.button9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(117, 29);
+            this.button9.TabIndex = 7;
+            this.button9.Text = "额外素材合并";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(143, 74);
+            this.button8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 29);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "RM动画导出";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(17, 74);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(117, 29);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "怪物数据导出";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(17, 168);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(203, 29);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "伤害和临界值计算器";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(17, 122);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 29);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "JS代码压缩";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(143, 25);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 29);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "地图生成器";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(17, 25);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 29);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "便捷PS工具";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 158);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "常用工具";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 61);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 15);
+            this.label3.TabIndex = 4;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(16, 212);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(101, 30);
+            this.button10.TabIndex = 8;
+            this.button10.Text = "动画编辑器";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(125, 210);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(136, 31);
+            this.button11.TabIndex = 9;
+            this.button11.Text = "怪物数据生成器";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 487);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "本地服务器";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading;
+using SimpleHttpServer;
+using SimpleHttpServer.Models;
+using SimpleHttpServer.RouteHandlers;
+
+
+namespace mota_js_server
+{
+    public partial class Form1 : Form
+    {
+        private Thread thread;
+
+        private int port;
+        private string url;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            port = 1055;
+            while (portInUse(port)) port++;
+
+            if (port > 1055)
+            {
+                MessageBox.Show("默认的1055端口已被占用，自动选择"+port+"端口。\n请注意，不同端口下的存档等信息都是不共用的。",
+                    "警告", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            url = "http://127.0.0.1:" + port + "/";
+
+            // 启动
+            HttpServer httpServer = new HttpServer(port, new List<Route>()
+            {
+                new Route()
+                {
+                    Callable = new FileSystemRouteHandler() {BasePath = ".", ShowDirectories = true}.Handle,
+                    UrlRegex = "^/(.*)$",
+                    Method = "GET"
+                },
+                new Route()
+                {
+                    Callable = MyRoute.route,
+                    UrlRegex = "^/(.*)$",
+                    Method = "POST"
+                },
+            });
+            thread = new Thread(new ThreadStart(httpServer.Listen));
+            thread.Start();
+            label1.Text = "已启动服务："+url;
+            // Current directory name
+            label3.Text = "当前目录："+Path.GetFileName(Directory.GetCurrentDirectory());
+            
+        }
+
+        private bool portInUse(int port)
+        {
+            IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            IPEndPoint[] ipEndPoints = ipGlobalProperties.GetActiveTcpListeners();
+            foreach (IPEndPoint ipEndPoint in ipEndPoints)
+            {
+                if (ipEndPoint.Port == port) return true;
+            }
+            return false;
+        }
+
+
+        private bool checkChrome()
+        {
+            RegistryKey browserKeys = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\Clients\StartMenuInternet");
+            if (browserKeys == null)
+                browserKeys = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Clients\StartMenuInternet");
+            string[] names = browserKeys.GetSubKeyNames();
+            foreach (string name in names)
+            {
+                if (name.ToLower().Contains("chrome"))
+                    return true;
+            }
+            return false;
+        }
+
+        private void openUrl(string url)
+        {
+            
+            if (checkChrome())
+            {
+                try
+                {
+                    Process.Start("chrome.exe", url);
+                    return;
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            MessageBox.Show("你当前没有安装Chrome浏览器，使用其他浏览器可能会导致本程序闪退或无法正常工作，强烈推荐下载Chrome浏览器后再进行操作。",
+                "警告", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            Process.Start(url);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openUrl(url + "index.html");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openUrl(url + "editor.html");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("常用工具\\便捷PS工具.exe"))
+            {
+                MessageBox.Show("找不到常用工具目录下的便捷PS工具！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Process.Start("常用工具\\便捷PS工具.exe");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("常用工具\\地图生成器.exe"))
+            {
+                MessageBox.Show("找不到常用工具目录下的地图生成器！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Process.Start("常用工具\\地图生成器.exe");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("常用工具\\JS代码压缩工具.exe"))
+            {
+                MessageBox.Show("找不到常用工具目录下的JS代码压缩工具！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Process.Start("常用工具\\JS代码压缩工具.exe");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("常用工具\\伤害和临界值计算器.exe"))
+            {
+                MessageBox.Show("找不到常用工具目录下的伤害和临界值计算器！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Process.Start("常用工具\\伤害和临界值计算器.exe");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("常用工具\\怪物数据导出器.exe"))
+            {
+                MessageBox.Show("找不到常用工具目录下的怪物数据导出器！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Process.Start("常用工具\\怪物数据导出器.exe");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                Environment.Exit(System.Environment.ExitCode);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("常用工具\\RM动画导出器.exe"))
+            {
+                MessageBox.Show("找不到常用工具目录下的RM动画导出器！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Process.Start("常用工具\\RM动画导出器.exe");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("常用工具\\额外素材合并工具.exe"))
+            {
+                MessageBox.Show("找不到常用工具目录下的额外素材合并工具！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Process.Start("常用工具\\额外素材合并工具.exe");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("常用工具\\动画编辑器.exe"))
+            {
+                MessageBox.Show("找不到常用工具目录下的动画编辑器！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Process.Start("常用工具\\动画编辑器.exe");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("常用工具\\怪物数据生成机.exe"))
+            {
+                MessageBox.Show("找不到常用工具目录下的怪物数据生成机！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Process.Start("常用工具\\怪物数据生成机.exe");
+        }
+    }
+}
